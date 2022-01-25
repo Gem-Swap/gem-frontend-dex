3a7dc885b6909ec97fc5ff96fac156504ebeeff2
Casweeney Ojukwu <casweeno2000@gmail.com>
2022-01-25 13:23:01 +0100

    Logo name changed

--- src/pages/RemoveLiquidity/index.tsx
+++ src/pages/RemoveLiquidity/index.tsx
@@ -123,7 +123,7 @@ export default function RemoveLiquidity({
       { name: 'verifyingContract', type: 'address' },
     ]
     const domain = {
-      name: 'WakandaSwap LPs',
+      name: 'GemSwap LPs',
       version: '1',
       chainId,
       verifyingContract: pair.liquidityToken.address,
